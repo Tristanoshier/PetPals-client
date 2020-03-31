4a88f183336c6b472c78f9936a9b0710267a5875
TaylorDickens <justindickens17@gmail.com>
2020-03-31 10:43:24 -0400

    taylor client side

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>lets begin</h1>
-     <h2>develop</h2>
+      <h1>lets begin</h1>
+      <h2>develop</h2>
+
+      <h3>taebae was here</h3>
     </div>
   );
 }
