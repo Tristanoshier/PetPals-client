7f5153d2e526614e46d591d2635f09ddba8ae5f6
TaylorDickens <justindickens17@gmail.com>
2020-04-01 11:25:04 -0400

    signup logic

--- src/components/auth/signup/Signup.tsx
+++ src/components/auth/signup/Signup.tsx
@@ -4,9 +4,49 @@ interface Props {
     isLogin: boolean,
     updateToken: (newToken: string) => void
 }
-export default class Signup extends React.Component<Props> {
-    render(){
-        return(
+
+interface State {
+    username: string,
+    password: string,
+    passwordRequired: boolean,
+    adoptionRecruiter: boolean
+}
+
+export default class Signup extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+    state = {
+        username: "",
+        password: "",
+        passwordRequired: false,
+        adoptionRecruiter: false
+    }
+
+    componentDidMount() {
+        let handleSubmit = (event: any) => {
+            event.preventDefault();
+            if (this.state.password.length < 5) {
+                this.setState({
+                    passwordRequired: true
+                })
+            } else {
+                fetch(``, {
+                    method: 'POST',
+                    body: JSON.stringify({ username: this.state.username, password: this.state.password }),
+                    headers: new Headers({
+                        'Content-Type': 'application/json'
+                    })
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        this.props.updateToken(data.sessionToken)
+                    })
+            }
+        }
+    }
+    render() {
+        return (
             <div>
                 <h1>Signup</h1>
             </div>
