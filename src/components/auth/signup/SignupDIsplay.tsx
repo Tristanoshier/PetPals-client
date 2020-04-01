68a5373a58c8c01fe451d691cef539b0d0306161
Tristanoshier <tsoshier@bsu.edu>
2020-04-01 13:37:24 -0400

    add to signupdisplay

---
+++ src/components/auth/signup/SignupDIsplay.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Form, FormGroup, Button, Label, Input } from 'reactstrap';
+
+export default function SignupDisplay() {
+    return (
+        <Form inline>
+            <FormGroup>
+                <Label for="unsername" hidden>Username</Label>
+                <Input type="text" name="username" id="username" placeholder="Username" />
+            </FormGroup>
+            <br />
+            <FormGroup>
+                <Label for="password" hidden>Password</Label>
+                <Input type="password" name="password" id="password" placeholder="Password" />
+            </FormGroup>
+            <br />
+            <Button>Submit</Button>
+        </Form>
+    )
+}
