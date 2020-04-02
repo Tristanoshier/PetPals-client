a50a8313a65c4ac0e0d56a5b1419e64d5a8a12ee
Tristanoshier <tsoshier@bsu.edu>
2020-04-02 10:42:24 -0400

    get toggle working

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import Login from './login/Login';
-import Signup from './signup/Signup'
-
-interface Props {
+import Signup from './signup/Signup';
+type State = {
+    isLogin: boolean
+}
+type Props = {
     updateToken: (newToken: string) => void
 }
-
-export default class Auth extends React.Component<Props> {
-    state = {
-        isLogin: false
+export default class Auth extends React.Component<Props, State> {
+    constructor(props: any){
+        super(props)
+        this.state = {
+            isLogin: true
+        }
     }
-
+    isLoginHandler() {
+        this.setState({
+            isLogin: !this.state.isLogin
+        })
+    }
     render(){
         return (
             <Container>
                 {this.state.isLogin ?
-                <Login isLogin={this.state.isLogin} updateToken={this.props.updateToken} /> : 
-                <Signup isLogin={this.state.isLogin} updateToken={this.props.updateToken} />
+                    <Login isLogin={this.state.isLogin} isLoginHandler={this.isLoginHandler.bind(this)} updateToken={this.props.updateToken}/> :
+                    <Signup isLogin={this.state.isLogin} isLoginHandler={this.isLoginHandler.bind(this)} updateToken={this.props.updateToken} />
                 }
             </Container>
         )
