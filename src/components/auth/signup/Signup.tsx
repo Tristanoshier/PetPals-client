b404a9cd918e9e0df69075110b8c58f823d8ce4a
TaylorDickens <justindickens17@gmail.com>
2020-04-02 14:58:45 -0400

    trying to update token

--- src/components/auth/signup/Signup.tsx
+++ src/components/auth/signup/Signup.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import SignupDisplay from './SignupDIsplay';
-=======
 import SignupDisplay from './SignupDIsplay'
->>>>>>> 9a4441bf0cc502f92e816f1be70db4bfc2c1afb4
-
 type Props = {
     isLogin: boolean,
     updateToken: (newToken: string) => void,
     isLoginHandler: () => void
 }
-
 type State = {
     username: string,
     password: string,
     passwordRequired: boolean,
     adoptionRecruiter: boolean,
 }
-
 export default class Signup extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
@@ -28,8 +21,6 @@ export default class Signup extends React.Component<Props, State> {
             adoptionRecruiter: false,
         }
     }
-
-
     handleSubmit = (event: any) => {
         event.preventDefault();
         if (this.state.password.length < 5) {
@@ -46,11 +37,10 @@ export default class Signup extends React.Component<Props, State> {
             })
                 .then(response => response.json())
                 .then(data => {
-                    this.props.updateToken(data.sessionToken)
+                    return this.props.updateToken(data.sessionToken)
                 })
         }
     }
-
     onUsernameChange(e: any) {
         this.setState({
             username: e.target.value
@@ -61,19 +51,16 @@ export default class Signup extends React.Component<Props, State> {
             password: e.target.value
         })
     }
-
     onAdoptionChangeTrue() {
         this.setState({
             adoptionRecruiter: true
         })
     }
-
     onAdoptionChangeFalse() {
         this.setState({
             adoptionRecruiter: false
         })
     }
-
     render() {
         return (
             <div>
--- src/components/auth/signup/SignupDIsplay.tsx
+++ src/components/auth/signup/SignupDIsplay.tsx
@@ -13,30 +13,30 @@ type Props = {
 }
 
 function SignupDisplay(props: Props) {
-        return (
-            <Form onChange={props.onChange}>
-                <FormGroup>
-                    <Label for="unsername" hidden>Username</Label>
-                    <Input onChange={e => props.onUsernameChange(e)} name="username" id="username" placeholder="Username" />
-                </FormGroup>
-                <br />
-                <FormGroup>
-                    <Label for="password" hidden>Password</Label>
-                    <Input onChange={e => props.onPasswordChange(e)} type="password" name="password" id="password" placeholder="Password" />
-                </FormGroup>
-                <br />
-                <ButtonGroup>
-                    <Label>Are you an adoption recruiter?</Label>
-                    <Button color="primary" onClick={() => props.onAdoptionChangeTrue()}>Yes</Button>
-                    <Button color="secondary" onClick={() => props.onAdoptionChangeFalse()}>No</Button>
-                </ButtonGroup>
-                <br />
-                <Button type="submit">Signup</Button>
-                <p className="auth-switch" onClick={props.isLoginHandler}>
-                    {props.isLogin ? "Don't have an account? Sign up here." : "Already have an account? Login here."}
-                </p>
-            </Form>
-        )
+    return (
+        <Form onChange={props.onChange}>
+            <FormGroup>
+                <Label for="unsername" hidden>Username</Label>
+                <Input onChange={e => props.onUsernameChange(e)} name="username" id="username" placeholder="Username" />
+            </FormGroup>
+            <br />
+            <FormGroup>
+                <Label for="password" hidden>Password</Label>
+                <Input onChange={e => props.onPasswordChange(e)} type="password" name="password" id="password" placeholder="Password" />
+            </FormGroup>
+            <br />
+            <ButtonGroup>
+                <Label>Are you an adoption recruiter?</Label>
+                <Button color="primary" onClick={() => props.onAdoptionChangeTrue()}>Yes</Button>
+                <Button color="secondary" onClick={() => props.onAdoptionChangeFalse()}>No</Button>
+            </ButtonGroup>
+            <br />
+            <Button type="submit">Signup</Button>
+            <p className="auth-switch" onClick={props.isLoginHandler}>
+                {props.isLogin ? "Don't have an account? Sign up here." : "Already have an account? Login here."}
+            </p>
+        </Form>
+    )
 }
 
 export default SignupDisplay;
