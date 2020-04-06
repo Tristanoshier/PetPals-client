36fed9f5d80f777e6c72189e57cad68ed315367d
TaylorDickens <justindickens17@gmail.com>
2020-04-06 16:25:51 -0400

    making posts modals

--- src/components/site/profile/post/PostCreate.tsx
+++ src/components/site/profile/post/PostCreate.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import AddIcon from '@material-ui/icons/Add';
-import IconButton from '@material-ui/core/IconButton/IconButton'
+import IconButton from '@material-ui/core/IconButton/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
 
 type Props = {
     postCreate: any,
     fetchPosts: () => void,
-    token: string
+    token: string,
+    createOff: () => void
 }
 
 type State = {
@@ -41,27 +43,37 @@ export default class PostCreate extends React.Component<Props, State> {
                         postUrl: ''
                     })
                     this.props.fetchPosts();
+                    this.props.createOff();
                 })
         } else {
             alert('Must add a caption to post')
         }
     }
 
+    closeCreateModal = () => {
+        this.props.createOff()
+    }
+
     render() {
         return (
-            <Container>
-                <Form onSubmit={this.handleSubmit} >
-                    <FormGroup>
-                        <Label htmlFor="posturl">PostUrl:</Label>
-                        <Input value={this.state.postUrl} onChange={e => this.setState({ postUrl: e.target.value })} />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label htmlFor="description">Caption:</Label>
-                        <Input value={this.state.description} onChange={e => this.setState({ description: e.target.value })} />
-                    </FormGroup>
-                    <IconButton type="submit">Add<AddIcon /></IconButton>
-                </Form>
-            </Container>
+            <>
+                <Modal isOpen={true}>
+                    <ModalHeader>Create Post <IconButton onClick={this.closeCreateModal.bind(this)}><ClearIcon /></IconButton></ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleSubmit} >
+                            <FormGroup>
+                                <Label htmlFor="posturl">PostUrl:</Label>
+                                <Input value={this.state.postUrl} onChange={e => this.setState({ postUrl: e.target.value })} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="description">Caption:</Label>
+                                <Input value={this.state.description} onChange={e => this.setState({ description: e.target.value })} />
+                            </FormGroup>
+                            <IconButton type="submit">Add<AddIcon /></IconButton>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </>
         )
     }
 }
