1e548031dc68b7aef0f4620e413658ee2923feca
Adam Clouse <amclouse96@gmail.com>
2020-04-06 13:08:03 -0400

    toggle profile components

--- src/components/site/profile/Profile.tsx
+++ src/components/site/profile/Profile.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
-import ProfileDisplay from './ProfileDisplay';
-import PostIndex from './post/PostIndex';
-import PetIndex from './pet/PetIndex';
+import React from "react";
+import ProfileDisplay from "./ProfileDisplay";
+import PostIndex from "./post/PostIndex";
+import PetIndex from "./pet/PetIndex";
 
 type Props = {
-    token: string
-}
+  token: string,
+};
 
-export default class Profile extends React.Component<Props> {
-    render() {
-        return (
-            <div>
-                <h1>Profile</h1>
-                <ProfileDisplay token={this.props.token} />
-                <PostIndex token={this.props.token}/>
-                <PetIndex token ={this.props.token} />
-            </div>
-        )
-    }
+type State = {
+  isToggle: boolean;
+};
+
+export default class Profile extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isToggle: true
+    };
+  }
+
+  isToggleHandler() {
+    this.setState({
+      isToggle: !this.state.isToggle
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Profile</h1>
+        <ProfileDisplay token={this.props.token} />
+
+        {this.state.isToggle ? (
+          <PostIndex token={this.props.token} />
+        ) : (
+          <PetIndex token={this.props.token} />
+        )}
+        <p className="auth-switch" onClick={this.isToggleHandler.bind(this)}>
+          {this.state.isToggle
+            ? "Post"
+            : "Pets"}
+        </p>
+      </div>
+    );
+  }
 }
