77d73db65085ae1ec9ef6ad86fc71543d0c633ee
TaylorDickens <justindickens17@gmail.com>
2020-04-14 17:54:17 -0400

    mobile profile

--- src/components/site/search/Search.tsx
+++ src/components/site/search/Search.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBarDisplay from './SearchBarDisplay';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 type Props = {
     token: string,
@@ -11,16 +11,17 @@ type Props = {
 export default class Search extends React.Component<Props> {
     render() {
         return (
+
             <Container className="search">
-                <SearchBarDisplay 
-                        token={this.props.token} 
-                        findAllUsers={this.props.findAllUsers}
-                        allUsers={this.props.allUsers}
-                    /> 
+                <SearchBarDisplay
+                    token={this.props.token}
+                    findAllUsers={this.props.findAllUsers}
+                    allUsers={this.props.allUsers}
+                />
                 <Row className="row-height">
-                    <Col className= "search-pic-wall-one" md="4"></Col>
-                    <Col className= "search-pic-wall-two" md="4"></Col>
-                    <Col className= "search-pic-wall-three" md="4"></Col>
+                    <Col className="search-pic-wall-one" md="4"></Col>
+                    <Col className="search-pic-wall-two" md="4"></Col>
+                    <Col className="search-pic-wall-three" md="4"></Col>
                 </Row>
             </Container>
         )
