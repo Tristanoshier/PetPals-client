a428c61485b9671879d333a81f5d23d88328b074
Tristanoshier <tsoshier@bsu.edu>
2020-04-15 15:50:31 -0400

    add reverse for initial state of the array

--- src/components/site/dashboard/postFeed/PostFeedIndex.tsx
+++ src/components/site/dashboard/postFeed/PostFeedIndex.tsx
@@ -27,7 +27,7 @@ export default class PostIndex extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            myFeed: [],
+            myFeed: [].reverse(),
             profileInfo: {}
         };
     }
